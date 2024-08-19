1657930f526d09ff51a792d1b128d12161b86cb6
davenfroberg <davenfroberg@gmail.com>
2024-08-19 15:02:20 -0600

    Changed favicon and made homepage look better

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -6,6 +6,10 @@ import './HomePage.css';
 const HomePage = () => {
     return (
         <div className="center-screen">
+            <div id="title">
+                <h1>Plethora File Storage</h1>
+                <h2>The Google Drive killer</h2>
+            </div>
             <div className="container">
                 <RegisterForm/>
                 <LoginForm/>
